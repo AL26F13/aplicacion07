de6bfe3545b4bfbabca83f0192185b24ac9f9681
fer montaño <fermonmtz@gmail.com>
2015-06-08 08:37:32 -0500

    seis
    modifique unos detalles

--- js/acciones.js
+++ js/acciones.js
@@ -1,31 +1,53 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	cargarnombrejugador();
+	function cargarnombrejugador (){
+		basedatos.transaction(function(ejecutar){
+			var sql="SELECT NombreUsuario FROM Usuario";
+			ejecutar.executeSql(sql,undefined,function(ejecutar,resultado){
+				var datosJugador=resultado.rows.item(0);
+				$('#jugador').text(datosJugador.NombreUsuario);
+			});
+		});
+	}
+	var basedatos=window.sqlitePlugin.openDatabase({name:"coloresBD.db",createFromLocation:1});
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX ('B1','audio/C.mp3',function(){},function(msg){ alert ("Error "+ msg);});
+	audio.preloadFX ('B2','audio/D.mp3',function(){},function(msg){ alert ("Error "+ msg);});
+    audio.preloadFX ('B3','audio/E.mp3',function(){},function(msg){ alert ("Error "+ msg);});
+	audio.preloadFX ('B4','audio/F.mp3',function(){},function(msg){ alert ("Error "+ msg);});		
 	$('#btnjugar').on('tap',function(){
 		var pantalla = $.mobile.getScreenHeight();
 		var encabezado = $('.ui-header').outerHeight(); 
 		var pie = $('.ui-footer').outerHeight();
 		var contenido = $('.ui-content').outerHeight();
-		var alto = (pantalla- encabezado - pie) / 2;
+		var alto = (pantalla- encabezado - pie)/2;
 		$('.cuadro').height(alto);
 		//alert('Pantalla '+ pantalla);
 		//alert('Encabezado '+ encabezado);
 });//btnjugar.click
     
-	function quien (q)
-	{
-		return q.substring(1);
-	}
-	
-   $('.cuadro').on('vmousedown',function(){
-	   $('#pantalla').append(quien($(this).attr('id')));
+	$('.cuadro').on('vmousedown',function(){
 	   $(this).addClass('pulsado');
    });
-   
    
     $('.cuadro').on('vmouseup',function(){
 	   $(this).removeClass('pulsado');
    });
+	function quien (q)
+	{
+		audio.play (q);
+		return q.substring(1);
+	}
+	
+   
+   
+	$('.cuadro').on('vmousedown',function(){
+	   $('#pantalla').append(quien($(this).attr('id')));
+	   $(this).addClass('pulsado');
+   });
+   
 }); 
 });
 
